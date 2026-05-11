55612a4e6ba846070108ddc611f49965ebf7edb6
oscarjimenezpuig <44938488+oscarjimenezpuig@users.noreply.github.com>
2026-05-11 13:26:53 +0200

    Add files via upload

--- ordenador.c
+++ ordenador.c
@@ -62,35 +62,91 @@ static u2 puncall(u1 nj,u1 cc) {
         puntos=(puntos==0)?100:puntos;
         tenido=NUMCASHOT+1;
     }
-    precio+=cll.alquiler[tenido];
+    puntos+=cll.alquiler[tenido];
     return (u2)puntos;
 }
         
-static void vecins(u1 c,u1 p,u1 dv,u1* v) {
-    /* se inserta en la posicion p del vector v el valor c,dv es la dimension de v */
-    for(int k=dv-1;k>=p;k--) v[k+1]=v[k];
-    v[p]=c;
-}      
-
-static u1 isgrt(u1 ca,u1 cb) {
+static u1 isgrt(u1 nj,u1 ca,u1 cb) {
     /* dice si la casilla ca es mayor que la casilla cb */
     Casilla a=tablero[ca];
     Casilla b=tablero[cb];
     if(a.tipo==NEGOCIO) return 1;
     else if(a.tipo==TRENES && b.tipo!=NEGOCIO) return 1;
     else if(a.tipo==CALLE && b.tipo==CALLE) {
-
+        return (puncall(nj,ca)>puncall(nj,cb));
     }
     return 0;
 }
 
-static void ordpos(u1* cd,u1 dco,u1* co) {
+static void ordpos(u1 nj,u1* cd,u1 dco,u1* co) {
     /* ordena las posesiones a partir de un baremo, primero los negocios y luego 
      * las estaciones y finalmente las calles ordenadas
      * cd es el vector ordenado, co es el vector desordenado, dco es la dimension */
-    u1* pco=co;
-    while(pco!=co+dco) {
-
-
-    
+    u1 ver[dco];
+    u1* pv=ver;
+    while(pv!=ver+dco) *pv++=0;
+    u1* pd=cd;
+    while(pd!=cd+dco) {
+        u1* max=NULL;
+        u1* pc=co;
+        pv=ver;
+        while(pc!=co+dco) {
+            if(!max || (*pv==0 && isgrt(nj,*pc,*max))) max=pc;
+            pc++;
+            pv++;
+        }
+        *pd++=*max;
+        *(ver+(max-co))=1;
+    }
 }
+
+static u1 jugest=0;
+static u1 condpro(Casilla* c) {
+    if(c && (c->tipo==CALLE || c->tipo==TRENES || c->tipo==NEGOCIO) && c.comprable.poseedor==jugest) return 1;
+    return 0;
+}
+
+static u1 getpos(u1 nj,u1* p) {
+    /* da todas las posesiones de un ordenador jugador */
+    jugest=nj;
+    u1* pp=p;
+    Casilla* pc=pttab(1,condpro);
+    while(pc) {
+        *pp++=*pc;
+        pc=pttab(0,condpro);
+    }
+    return pp-p;
+}
+
+static void ordenador_vender(u1 nj) {
+    /* el ordenador ha decidido vender una de sus posesiones */
+    u1 caspos[TABSIZ];
+    u1 size=getpos(nj,caspos);
+    if(size) {
+        u1 ocaspos[size];
+        ordpos(nj,ocaspos,size,caspos);
+        u1 cav=ocaspos[size-1];
+        Jugador j=jugadores[nj];
+        Casilla c=tablero[cav];
+        prt("%s ha vendido %s.",j.nombre,c.nombre);
+        nln;
+        vender(nj,cav);
+    }
+}
+
+static void ordenador_comprar(u1 nj,u1 nc) {
+    /* el ordenador intenta comprar una casilla comprable */
+    Jugador j=jugadores[nj];
+    Casilla c=tablero[nc];
+    Comprable cc=c.comprable;
+    int prac=j.dinero-cc.precio;
+    if(prac>=j.riesgo) {
+        prt("%s compra %s.",j.nombre,c.nombre);
+        nln;
+        comprar(nj);
+    } else {
+        prt("%s no compra %s, la propiedad pasara a subastarse.",j.nombre,c.nombre);
+        subasta_flag_on(nj);
+    }
+}
+    
